3e68a6a301a224757466d1ef7c8c34bf726de139
Marko Sarkanj <marko.sarkanj@gmail.com>
2017-12-05 11:05:42 +0100

    lecture 170

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 class App extends Component {
+  // state = {
+  //   show: true
+  // };
+
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({ show: false });
+  //   }, 5000);
+  // }
+
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder> </BurgerBuilder>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
